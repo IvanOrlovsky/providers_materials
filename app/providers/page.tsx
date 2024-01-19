2621e587973495be5173d5e181c321fcec954c41
IvanOrlovsky <iiiaaaiii706@gmail.com>
2024-01-19 17:22:46 +0300

    made working table

--- app/providers/page.tsx
+++ app/providers/page.tsx
@@ -4,14 +4,11 @@ import { getAllProviders } from "../db/queries"
 import Table from "@/components/Table/table";
 
 export default async function Providers() {
+    
     let { rows } = await getAllProviders();
 
     return (
         <div>
-            <h1>Поставщики</h1>
-            <p>
-                {JSON.stringify(rows)}
-            </p>
             <Table entries={rows}/>
         </div>
     )
--- components/Table/table.tsx
+++ components/Table/table.tsx
@@ -5,10 +5,29 @@ interface TableProps {
 }
 
 export default function Table({ entries }: TableProps) {
-    const a = entries;
+
+    const keys = Object.keys(entries[0]);
+    
     return (
         <div>
-            {JSON.stringify(a)}
+            <table className="table">
+                <thead>
+                    <tr>
+                        {
+                            keys.map((key) => (<th key={key}>{key}</th>))
+                        }
+                    </tr>
+                </thead>
+                <tbody>
+                        {entries.map((entry, index) => (
+                                <tr key={index}>
+                                    {keys.map((key) => (
+                                        <td key={key}>{entry[key]}</td>
+                                    ))}
+                                </tr>
+                            ))}
+                </tbody>
+            </table>
         </div>
     );
 }
