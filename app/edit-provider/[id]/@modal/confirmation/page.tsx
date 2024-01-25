04e208bf3a47c1cc4adc735d299d835e11dbf78a
IvanOrlovsky <iiiaaaiii706@gmail.com>
2024-01-25 17:49:38 +0300

    made tables on confirmaton modal

--- app/edit-provider/[id]/@modal/confirmation/page.tsx
+++ app/edit-provider/[id]/@modal/confirmation/page.tsx
@@ -4,17 +4,105 @@ import Modal from "@/components/Modal/Modal";
 import { useSearchParams } from "next/navigation";
  
 
-export default function Confirmation() {
+export default function Confirmation({ params } : { params: { id: string } }) {
 
     const searchParams = useSearchParams();
-    console.log(searchParams.get("prevProviderInfo"))
 
+    const prevProviderMaterialsInfo = JSON.parse(searchParams.get("prevProviderMaterialsInfo") as string)
+    const providerMaterialsDisabledRows = searchParams.get("providerMaterialsDisabledRows") ? 
+        JSON.parse(searchParams.get("providerMaterialsDisabledRows") as string) : []
+    const providerMaterialsQuantities = searchParams.get("providerMaterialsQuantities")
+    const prevProviderInfo = JSON.parse(searchParams.get("prevProviderInfo") as string)
+    const providerId = searchParams.get("providerId")
+    const providerType = searchParams.get("providerType")
+    const providerName = searchParams.get("providerName")
+    const providerNumber = searchParams.get("providerNumber")
+    const providerAddress = searchParams.get("providerAddress")
+
+    console.log(providerMaterialsDisabledRows)
+    
     return (
         <Modal
-        title='qwe'
+        title='Подтвердите изменения'
         >
-            <>
-            </>
+            <section className="modal-card-body">
+                <div className="tile is-ancestor">
+                    <div className="tile is-child box">
+                    <table className="table is-bordered my-1 is-narrow">
+                        <tbody>
+                            <tr>
+                                <th>
+                                    Номер поставщика
+                                </th>
+                                <td className={prevProviderInfo['Номер поставщика'] != providerId?
+                                "is-warning" : ""}>
+                                    {providerId}
+                                </td>
+                            </tr>
+                            <tr>
+                                <th>
+                                    Тип поставщика
+                                </th>
+                                <td className={prevProviderInfo["Тип поставщика"] != providerType?
+                                "is-warning" : ""}>
+                                    {providerType}
+                                </td>
+                            </tr>
+                            <tr>
+                                <th>
+                                    Название компании
+                                </th>
+                                <td className={prevProviderInfo['Название компании'] != providerName?
+                                "is-warning" : ""}>
+                                    {providerName}
+                                </td>
+                            </tr>
+                            <tr>
+                                <th>
+                                    Номер телефона
+                                </th>
+                                <td className={prevProviderInfo['Номер телефона'] != providerNumber?
+                                "is-warning" : ""}>
+                                    {providerNumber}
+                                </td>
+                            </tr>
+                            <tr>
+                                <th>
+                                    Адрес
+                                </th>
+                                <td className={prevProviderInfo['Адрес'] != providerAddress?
+                                "is-warning" : ""}>
+                                    {providerAddress}
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    </div>
+                    <div className="tile is-child box">
+                        <table className="table is-bordered my-5"> 
+                            <tbody>
+                                {prevProviderMaterialsInfo.map((material: {[key: string] : string}) => (
+                                    <tr
+                                    key={material["Номер материала"]}
+                                    style={providerMaterialsDisabledRows.includes(+material["Номер материала"]) ? { backgroundColor: 'rgba(255, 0, 0, 0.5)' } : {}}
+                                    >
+                                        <th className="">
+                                            {material["Название материала"]}
+                                        </th>
+                                        <td>
+                                            {material["Количество"]}
+                                        </td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </section>
+            <footer className="modal-card-foot">
+                <button className="button is-success">Save changes</button>
+                <button className="button">Cancel</button>
+            </footer>
         </Modal>
     )
 }
