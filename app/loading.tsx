6df471aae6bfe618d7069e84a3ff748de826dd73
IvanOrlovsky <iiiaaaiii706@gmail.com>
2024-01-26 17:22:34 +0300

    made lodaing page for each page

---
+++ app/loading.tsx
@@ -0,0 +1,13 @@
+
+export default function Loading() {
+    
+    return (
+        <section className="section">
+            <div className="container is-size-3">
+                <div className="columns is-centered">
+                        <span className="loader"></span>
+                </div>
+            </div>
+      </section>
+    )
+  }
