b659db3d6dd212e37be6accf8b0ec4c674486894
IvanOrlovsky <iiiaaaiii706@gmail.com>
2024-01-30 09:05:34 +0300

    removed modal from materials layout

--- app/materials/layout.tsx
+++ app/materials/layout.tsx
@@ -1,24 +1,15 @@
-import MaterialsContextProvider from "@/contexts/MaterialsContext";
-
 /**
  * Слой для станицы материалов
  * @param children дочерний компонент
- * @param materials_modal модальные окна из параллельного маршрута
- *
  */
 export default function MaterialsLayout({
 	children,
-	materials_modal,
 }: {
 	children: React.ReactNode;
-	materials_modal: React.ReactNode;
 }) {
 	return (
 		<>
-			<MaterialsContextProvider>
-				<div>{children}</div>
-				<div>{materials_modal}</div>
-			</MaterialsContextProvider>
+			<div>{children}</div>
 		</>
 	);
 }
